ac8eafccfa27905116020416e368482b7f5b5d43
rwinkah <rwinosa@gmail.com>
2024-03-19 21:27:16 +0100

    update production

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import DashboardLayout from "./dashboardLayout";
 
 export default function Dashboard() {
     return (<>
-       <DashboardLayout />
+        <DashboardLayout activePage='home'>
+            <div>Pluto</div>
+       </DashboardLayout>
     </>)
 }
