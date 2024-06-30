0c2d3012ee3618c89cd51ef936f0050ee3a82773
rwinkah <rwinosa@gmail.com>
2024-06-30 00:02:32 +0000

    update: license

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <RespContainer hide={true} class_full="">
       <footer className="py-5 mt-5 pb-0 mb-0 ">
         <p className="text-foreground text-xs mb-0 pb-0">
-          Copyright © 2024 CPE E-Learning Uniben, All rights reserved.
+          © 2024 CPE E-Learning Uniben. This software is licensed under the MIT License.
         </p>
       </footer>
     </RespContainer>
